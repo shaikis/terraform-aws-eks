4a74ef35e9554029bb70281ea2bd618330810a83
Ismail Shaik <60819791+shaikis@users.noreply.github.com>
2021-11-06 11:58:27 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,10 @@
-output "cluster_endpoint" {
+output "eks_cluster_endpoint" {
   value = aws_eks_cluster.eks_cluster.endpoint
 }
-output "cluster_certificate_authority_data" {
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
+}
+output "eks_cluster_certificate_authority_data" {
   value = aws_eks_cluster.eks_cluster.certificate_authority.0.data
 }
 output "cluster_securitygroup_id" {
